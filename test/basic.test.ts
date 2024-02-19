ce1f4ae01f71557c75cbe3b088ec7cb895af8d55
Daniel Roe <daniel@roe.dev>
2024-02-19 23:34:19 +0000

    test: update test cases

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -6,9 +6,16 @@ await setup({
   rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
 })
 
-describe('ssr', async () => {
-  it('renders the index page', async () => {
+describe('providers', async () => {
+  it('generates inlined font face rules for `google` provider', async () => {
     const html = await $fetch('/')
-    expect(html).toContain('Nuxt module playground!')
+    for (const font of ['Poppins', 'Raleway', 'Montserrat']) {
+      expect(html).toContain(`@font-face {\n  font-family: '${font}';\n  src: local("${font}"), url`)
+    }
+  })
+
+  it('generates inlined font face rules for `local` provider', async () => {
+    const html = await $fetch('/')
+    expect(html).toContain('src: url("/custom-font.woff2") format(woff2)')
   })
 })
