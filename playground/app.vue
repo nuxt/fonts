9771234dc2af88918ef2068adde7290dbe8edfd6
Daniel Roe <daniel@roe.dev>
2024-02-21 10:47:23 +0000

    chore: add links to playground

---
+++ playground/app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const routes = useRouter().getRoutes().sort((a, b) => a.path.localeCompare(b.path))
+</script>
+
+<template>
+  <div class="p-8">
+    <ul>
+      <li
+        v-for="route in routes"
+        :key="route.path"
+      >
+        <NuxtLink
+          :to="route.path"
+          active-class="font-bold"
+          no-prefetch
+        >
+          {{ route.path }}
+        </NuxtLink>
+      </li>
+    </ul>
+    <hr class="my-8">
+    <NuxtPage />
+  </div>
+</template>
+
