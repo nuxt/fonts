b7646e93fd2557c0d3d653b2965169a901b5d86d
Daniel Roe <daniel@roe.dev>
2024-09-11 22:42:34 +0100

    fix: inject relative font urls in css
    resolves https://github.com/nuxt/fonts/issues/147

--- test/utils.ts
+++ test/utils.ts
@@ -1,8 +1,8 @@
 export function extractFontFaces(fontFamily: string, html: string) {
   const matches = html.matchAll(new RegExp(`@font-face\\s*{[^}]*font-family:\\s*(?<quote>['"])?${fontFamily}\\k<quote>[^}]+}`, 'g'))
   return Array.from(matches, match => match[0]
-    .replace(/(?<=['"(])(https?:\/\/[^/]+|\/_fonts)\/[^")]+(\.[^".)]+)(?=['")])/g, '$1/file$2')
-    .replace(/(?<=['"(])(https?:\/\/[^/]+|\/_fonts)\/[^".)]+(?=['")])/g, '$1/file'),
+    .replace(/(?<=['"(])(https?:\/\/[^/]+|(?:..)?\/_fonts)\/[^")]+(\.[^".)]+)(?=['")])/g, '$1/file$2')
+    .replace(/(?<=['"(])(https?:\/\/[^/]+|(?:..)?\/_fonts)\/[^".)]+(?=['")])/g, '$1/file'),
   )
 }
 
