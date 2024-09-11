f4cf7e8461227f42641c0cb69f984c4e9ada41b1
Tom Tang <me@qwerzl.me>
2024-09-12 06:00:39 +0800

    feat: `googleicons` provider (#133)

---
+++ playground/pages/providers/googleicons.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    Material Symbols Outlined:
+    <div class="material-symbols-outlined">
+      search
+    </div>
+    Material Icons Outlined:
+    <div class="material-icons-outlined">
+      search
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.material-symbols-outlined {
+  font-family: 'Material Symbols Outlined';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+}
+
+.material-icons-outlined {
+  font-family: 'Material Icons Outlined';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+}
+</style>
