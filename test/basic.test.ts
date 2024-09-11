572f3caa25f1ee2e98c99516a15be2a885730e38
Daniel Roe <daniel@roe.dev>
2024-09-11 21:42:39 +0100

    test: extract test helpers

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,6 +2,8 @@ import { fileURLToPath } from 'node:url'
 import { describe, it, expect } from 'vitest'
 import { setup, $fetch } from '@nuxt/test-utils'
 
+import { extractFontFaces, extractPreloadLinks } from './utils'
+
 await setup({
   rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
 })
@@ -238,15 +240,3 @@ describe('features', () => {
     `)
   })
 })
-
-function extractFontFaces(fontFamily: string, html: string) {
-  const matches = html.matchAll(new RegExp(`@font-face\\s*{[^}]*font-family:\\s*(?<quote>['"])?${fontFamily}\\k<quote>[^}]+}`, 'g'))
-  return Array.from(matches, match => match[0]
-    .replace(/(?<=['"(])(https?:\/\/[^/]+|\/_fonts)\/[^")]+(\.[^".)]+)(?=['")])/g, '$1/file$2')
-    .replace(/(?<=['"(])(https?:\/\/[^/]+|\/_fonts)\/[^".)]+(?=['")])/g, '$1/file'),
-  )
-}
-
-function extractPreloadLinks(html?: string) {
-  return (html?.match(/<link[^>]+rel="preload"[^>]+>/g) || []).map(link => link.match(/href="([^"]+)"/)?.[1]?.replace(/\/_fonts\/[^")]+(\.[^".)]+)$/g, '/file$1'))
-}
---
+++ test/utils.ts
@@ -0,0 +1,11 @@
+export function extractFontFaces(fontFamily: string, html: string) {
+  const matches = html.matchAll(new RegExp(`@font-face\\s*{[^}]*font-family:\\s*(?<quote>['"])?${fontFamily}\\k<quote>[^}]+}`, 'g'))
+  return Array.from(matches, match => match[0]
+    .replace(/(?<=['"(])(https?:\/\/[^/]+|\/_fonts)\/[^")]+(\.[^".)]+)(?=['")])/g, '$1/file$2')
+    .replace(/(?<=['"(])(https?:\/\/[^/]+|\/_fonts)\/[^".)]+(?=['")])/g, '$1/file'),
+  )
+}
+
+export function extractPreloadLinks(html?: string) {
+  return (html?.match(/<link[^>]+rel="preload"[^>]+>/g) || []).map(link => link.match(/href="([^"]+)"/)?.[1]?.replace(/\/_fonts\/[^")]+(\.[^".)]+)$/g, '/file$1'))
+}
