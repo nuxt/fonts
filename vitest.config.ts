36e5e0863fec6897c52a6be05f31a53198c9062c
Daniel Roe <daniel@roe.dev>
2024-10-23 09:44:59 +0100

    test: set `coverage.include` path

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globalSetup: './test/setup.ts',
+    coverage: {
+      include: ['src'],
+    },
   },
 })
