45118e80552ac9ef9c58e6ab44157093264418a0
Daniel Roe <daniel@roe.dev>
2025-04-24 15:34:52 +0200

    chore: update poppins font face length

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -106,7 +106,7 @@ describe('providers', async () => {
     const poppins = extractFontFaces('Poppins', html)
     const raleway = extractFontFaces('Raleway', html)
     const press = extractFontFaces('Press Start 2P', html)
-    expect(poppins.length).toMatchInlineSnapshot(`6`)
+    expect(poppins.length).toMatchInlineSnapshot(`8`)
     // No `@font-face` is generated for second/fallback fonts
     expect(raleway.length).toMatchInlineSnapshot(`0`)
     expect(poppins[0]).toMatchInlineSnapshot(`"@font-face{font-family:Poppins;src:local("Poppins Regular Italic"),local("Poppins Italic"),url(/_fonts/file.woff2) format(woff2);font-display:swap;unicode-range:U+0100-02BA,U+02BD-02C5,U+02C7-02CC,U+02CE-02D7,U+02DD-02FF,U+0304,U+0308,U+0329,U+1D00-1DBF,U+1E00-1E9F,U+1EF2-1EFF,U+2020,U+20A0-20AB,U+20AD-20C0,U+2113,U+2C60-2C7F,U+A720-A7FF;font-weight:400;font-style:italic}"`)
