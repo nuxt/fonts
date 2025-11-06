c9cc28de6466a020d89e8f1c18120972ea1d9566
Daniel Roe <daniel@roe.dev>
2025-11-06 22:16:27 +0000

    chore(deps): upgrade fontaine + fontless

--- test/css-frameworks/unocss.test.ts
+++ test/css-frameworks/unocss.test.ts
@@ -15,6 +15,6 @@ describe('unocss features', () => {
     const css = await $fetch<string>(cssFile)
     const barlow = extractFontFaces('Barlow', css)
     expect(barlow.length).toMatchInlineSnapshot(`8`)
-    expect(barlow[0]).toMatchInlineSnapshot(`"@font-face{font-family:Barlow;src:local("Barlow Regular Italic"),local("Barlow Italic"),url(../_fonts/file.woff2) format(woff2);font-display:swap;unicode-range:U+0102-0103,U+0110-0111,U+0128-0129,U+0168-0169,U+01A0-01A1,U+01AF-01B0,U+0300-0301,U+0303-0304,U+0308-0309,U+0323,U+0329,U+1EA0-1EF9,U+20AB;font-weight:400;font-style:italic}"`)
+    expect(barlow[0]).toMatchInlineSnapshot(`"@font-face{font-family:Barlow;src:local("Barlow Regular Italic"),local("Barlow Italic"),url(../_fonts/file.woff) format(woff);font-display:swap;font-weight:400;font-style:italic}"`)
   })
 })
