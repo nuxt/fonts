nuxt/fonts

commit 0ec437e7732e15cfe3f4b7466dbfc9de1f99cb78

fix: prevent font flashes in development
resolves https://github.com/nuxt/fonts/issues/181

src/assets.ts
@@ -2,7 +2,7 @@ import fsp from 'node:fs/promises'
 import { writeFileSync } from 'node:fs'
 import { addDevServerHandler, addVitePlugin, useNuxt } from '@nuxt/kit'
 import type { H3Event } from 'h3'
-import { eventHandler, createError } from 'h3'
+import { eventHandler, createError, setResponseHeader } from 'h3'
 import { $fetch } from 'ofetch'
 import { colors } from 'consola/utils'
 import { defu } from 'defu'
@@ -28,7 +28,7 @@ export async function setupPublicAssetStrategy(options: ModuleOptions['assets'] 
   nuxt.hook('modules:done', () => nuxt.callHook('fonts:public-asset-context', context))
 
   // Register font proxy URL for development
-  async function devEventHandler(event: { path: string }) {
+  async function devEventHandler(event: H3Event) {
     const filename = event.path.slice(1)
     const url = context.renderedFontURLs.get(event.path.slice(1))
     if (!url) {
@@ -41,6 +41,8 @@ export async function setupPublicAssetStrategy(options: ModuleOptions['assets'] 
       res = await $fetch(url, { responseType: 'arrayBuffer' }).then(b => Buffer.from(b))
       await storage.setItemRaw(key, res)
     }
+    // Set immutable cache headers to prevent font flashes during development
+    setResponseHeader(event, 'Cache-Control', 'public, max-age=31536000, immutable')
     return res
   }
 

commit 3ab91c7aad516631744a97d0249b70e37790f1ff

fix: pass a real H3Event in Storybook mode
Restores Storybook compatibility. h3 was trying to set the cache header,
but the injected handler in Storybook mode did not pass the req/res
objects. This was crashing the Storybook server on start as soon as a
font was loaded.

Regression introduced in 0ec437e7732e15cfe3f4b7466dbfc9de1f99cb78.

Signed-off-by: Cynthia Rey <cynthia.rey@irit.fr>

src/assets.ts
@@ -2,7 +2,7 @@ import fsp from 'node:fs/promises'
 import { writeFileSync } from 'node:fs'
 import { addDevServerHandler, addVitePlugin, useNuxt } from '@nuxt/kit'
 import type { H3Event } from 'h3'
-import { eventHandler, createError, setResponseHeader } from 'h3'
+import { eventHandler, createEvent, createError, setResponseHeader } from 'h3'
 import { $fetch } from 'ofetch'
 import { colors } from 'consola/utils'
 import { defu } from 'defu'
@@ -58,7 +58,10 @@ export async function setupPublicAssetStrategy(options: ModuleOptions['assets'] 
       if (server.config.appType !== 'custom' || nuxt.options.buildId === 'storybook') {
         server.middlewares.use(
           context.assetsBaseURL,
-          async (req, res) => { res.end(await devEventHandler({ path: req.url } as H3Event)) },
+          async (req, res) => {
+            const h3evt = createEvent(req, res)
+            res.end(await devEventHandler(h3evt))
+          },
         )
       }
     },