7995bd154aca243c1096dcb501300b4ff0eb3dfd
Daniel Roe <daniel@roe.dev>
2026-09-03 08:16:40 +0000

    test: clear the font cache of every fixture and playground

--- test/setup.ts
+++ test/setup.ts
@@ -1,11 +1,16 @@
 import { promises as fsp } from 'node:fs'
 
+import { glob } from 'tinyglobby'
+
 import { cacheBase } from '../src/cache'
 
 export async function setup() {
-  await fsp.rm('./' + cacheBase, { recursive: true, force: true })
-  for (const dir of ['basic', 'nuxt5', 'scss', 'tailwindcss', 'unocss']) {
-    await fsp.rm('./playgrounds/' + dir + '/' + cacheBase, { recursive: true, force: true })
+  const caches = await glob([cacheBase, `{playgrounds,test/fixtures}/*/${cacheBase}`], {
+    onlyDirectories: true,
+    absolute: true,
+  })
+  for (const cache of caches) {
+    await fsp.rm(cache, { recursive: true, force: true })
   }
   console.log('✅ Cleared font cache.')
 }
